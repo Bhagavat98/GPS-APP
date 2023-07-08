27f5845c391aa21bc59b489cb410ce57c80522bc
Bhagavat98 <56771638+Bhagavat98@users.noreply.github.com>
2023-07-08 15:11:40 +0530

    Update App.js
    ddd

--- App.js
+++ App.js
@@ -23,3 +23,16 @@ class StarterApp extends React.Component {
 AppRegistry.registerComponent("StarterApp", () => StarterApp);
 
 export default StarterApp;
+
+01-Aug-2022 - 35925.0
+02-Sep-2022 - 49598.00
+04-Oct-2022 - 49598.00
+01-Nov-2022 - 49597.00
+01-Dec-2022 - 51337.00
+30-Dec-2022 - 51325.00
+01-Feb-2023 - 45859.00
+01-Mar-2023 - 51237.00
+31-Mar-2023 - 51337.00
+01-May-2023 - 51337.00
+01-Jun-2023 - 51337.00
+01-Jul-2023 - 51325.00
