13a9e0369ac5265a173b671196a8ec5caaf65c5e
Bhagavat98 <56771638+Bhagavat98@users.noreply.github.com>
2024-03-29 10:42:33 +0530

    Update App.js
    dd

--- App.js
+++ App.js
@@ -6,3 +6,9 @@ https://apps.randstad.in/ourpeople/
 प्रधानमंत्री कुसुम घटक-ब योजनेअंतर्गत सौर कृषिपंप करिता आपल्या अर्जाची माहिती https://kusumbenef.mahadiscom.in/benef_home या लिंकवर लॉगइन करून पूर्ण भरावी. धन्यवाद.महावितरण
 नोंदणी झालेल्या अर्जाची माहिती खालील प्रमाणे,
 Application No.: MS230518432 , Username: MS230518432 , Password: 9139
+
+<div class="btn_brnf">
+  <a class="btn btn-sm btn-outline-primary" target="_blank" onclick="printTapshil1(9,18432)">
+    <i class="fa fa-file" aria-hidden="true"></i>&nbsp;कोटेशन
+  </a>
+</div>
