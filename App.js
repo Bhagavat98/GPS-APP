304791031e1396ba4678f728037a05b067588e4c
Bhagavat98 <56771638+Bhagavat98@users.noreply.github.com>
2024-07-10 20:13:27 +0530

    Update App.js
    dd

--- App.js
+++ App.js
@@ -1,14 +1,1 @@
-chetan.patil@mahindraholidays.com
-gurpreet.panesar@mahindraholidays.com
-bhagavat.solanke550@mahindraholidays.com
-
-Request for Work from Home for Medical Recovery
-
-Hi Chetan Sir and Gurpreet Sir,
-
-     As you know I had met with an major accident last year due to which a surgery was required to fix the plate in my right hand as it was totally damaged ,as per doctors suggestion I am not allowed to drive or lift heavy objects until the plate is removed,  so I kindly request you to give me some time to work from home till February/10/2025. 
-
-Please find the attached surgery reports for your reference.
-
-Thanks & Regards
-Bhagavat Solanke
+dd
